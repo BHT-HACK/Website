2a431324d9afb6830a6d81bb9fa5b0880a8e84d3
Divyam Tayal <daretobedifferent10920@gmail.com>
2020-09-12 03:57:49 +0530

    Added MyProfile and Registration Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import MyProfile from "./components/MyProfile/MyProfile";
+import Registration from "./components/Registration/Registration";
 
 function App() {
   return (
     <div className="App">
-  
+      <MyProfile />
+      <Registration />
     </div>
   );
 }
